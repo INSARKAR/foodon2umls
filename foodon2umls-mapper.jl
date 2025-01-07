ae4a852b2b96626354b9197cc73397ddc67e8ffd
INSARKAR <neil_sarkar@brown.edu>
2025-01-07 12:49:52 -1000

    first full mapping

--- foodon2umls-mapper.jl
+++ foodon2umls-mapper.jl
@@ -36,6 +36,8 @@ function main()
     # Define the dictionary to store the mappings
     foodon2umls_mapping = Dict{String, String}()
 
+    # Create a set to keep track of FoodOn IDs
+
     # Load the foodon terms into a dictionary
     foodon_terms = Dict{String, String}()
     open(foodon_terms_file, "r") do file
@@ -93,6 +95,10 @@ function main()
     # Initialize the counter
     counter = 0
 
+    # Create a set to keep track of FoodOn IDs
+    processed_foodon_ids = Set{String}()
+
+
     # If the mapping file exists, read in the UMLS CUIs into a set
     mapped_umls_cuis = Set{String}()
     if isfile(foodon2umls_mapping_file_name)
@@ -101,6 +107,10 @@ function main()
                 parts = split(line, '|')
                 umls_cui = parts[5]
                 push!(mapped_umls_cuis, umls_cui)
+
+                foodon_id = parts[3]
+                push!(processed_foodon_ids, foodon_id)
+
             end
         end
     end
@@ -109,9 +119,12 @@ function main()
     mapped_umls_cuis_count = length(mapped_umls_cuis)
     println("$mapped_umls_cuis_count UMLS CUIs already mapped.")
 
+
+    foodon2umls_mapping_file = open(foodon2umls_mapping_file_name, "a")
+
     if mapped_umls_cuis_count != mapped_umls_cuis_count
     
-        foodon2umls_mapping_file = open(foodon2umls_mapping_file_name, "a")
+
 
         # Iterate over the UMLS terms and find direct mappings (UMLS term == FoodOn term)
         for (umls_term, umls_cui) in umls_terms
@@ -127,7 +140,7 @@ function main()
 
             # direct mappings
             if haskey(foodon_terms, umls_term)
-                println("D $counter Found direct mapping for term (U>F): $umls_term")
+                println("D $counter / $umls_terms_count Found direct mapping for term (U>F): $umls_term")
                 foodon2umls_mapping[foodon_terms[umls_term]] = umls_cui
                 print(foodon2umls_mapping_file, "D|U>F|$(foodon_terms[umls_term])|$umls_term|$umls_cui|$umls_term\n")
                 # exit()
@@ -151,16 +164,16 @@ function main()
                         first_result = results["response"]["docs"][1]
                         foodon_id = first_result["short_form"]
                         foodon_term = first_result["label"]
-                        println("O $counter Found OLS mapping for term (U>F): $umls_term -> $foodon_term [$foodon_id]")
+                        println("O $counter / $umls_terms_count Found OLS mapping for term (U>F): $umls_term -> $foodon_term [$foodon_id]")
                         foodon2umls_mapping[foodon_id] = umls_cui
                         print(foodon2umls_mapping_file, "O|U>F|$foodon_id|$foodon_term|$umls_cui|$umls_term\n")
                         #exit()
                     else
-                        println("N $counter No OLS mapping found for term (U>F): $umls_term")
+                        println("N $counter / $umls_terms_count No OLS mapping found for term (U>F): $umls_term")
                         print(foodon2umls_mapping_file, "N|U>F|||$umls_cui|$umls_term\n")
                     end
                 else
-                    println("N $counter Failed to query OLS for term (U>F): $umls_term")
+                    println("N $counter / $umls_terms_count Failed to query OLS for term (U>F): $umls_term")
 
                     print(foodon2umls_mapping_file, "N|U>F|||$umls_cui|$umls_term\n")
                     push!(non_mapped_terms, umls_term)
@@ -174,6 +187,61 @@ function main()
     else
         println("All (U>F) terms have been mapped.")
     end
+
+
+    counter = 0
+
+    # Iterate over the FoodOn terms and find mappings using UMLS API
+    for (foodon_term, foodon_id) in foodon_terms
+
+        counter += 1
+
+        if foodon_id in values(foodon2umls_mapping)
+            continue
+        end
+
+        if foodon_id in processed_foodon_ids
+            continue
+        end
+
+        # Encode the term to be URL-safe
+        encoded_foodon_term = HTTP.escape(foodon_term)
+
+        umls_api_key = "3978016a-b02f-4cc4-ae31-cf161882931a"
+
+        umls_url = "https://uts-ws.nlm.nih.gov/rest/search/current?string=$encoded_foodon_term&searchType=normalizedString&apiKey=$umls_api_key"
+        #println(umls_url)
+
+        response = HTTP.get(umls_url)
+        #println(response)
+
+        if response.status == 200
+            results = JSON.parse(String(response.body))
+            if !isempty(results["result"]["results"])
+                first_result = results["result"]["results"][1]
+                umls_cui = first_result["ui"]
+                umls_term = first_result["name"]
+                println("U $counter / $foodon_terms_count Found UMLS mapping for term (F>U): $foodon_term -> $umls_term [$umls_cui]")
+                foodon2umls_mapping[foodon_id] = umls_cui
+                print(foodon2umls_mapping_file, "U|F>U|$foodon_id|$foodon_term|$umls_cui|$umls_term\n")
+                
+
+            else
+                println("N $counter / $foodon_terms_count No UMLS mapping found for term (F>U): $foodon_term")
+                print(foodon2umls_mapping_file, "N|F>U|$foodon_id|$foodon_term||\n")
+            end
+        else
+            println("N $counter / $foodon_terms_count Failed to query UMLS for term (F>U): $foodon_term")
+            print(foodon2umls_mapping_file, "N|F>U|$foodon_id|$foodon_term||\n")
+            push!(non_mapped_terms, foodon_term)
+        end
+    end
+
+
+    # close the mapping file
+    close(foodon2umls_mapping_file)
+
+
 end
 
 main()
